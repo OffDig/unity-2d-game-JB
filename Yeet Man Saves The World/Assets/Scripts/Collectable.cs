4f451f51ddc32ce5235acab7e8cbc011f70221bd
OffDig <55810522+OffDig@users.noreply.github.com>
2019-09-25 15:38:30 -0500

    Added Game Folder

---
+++ Yeet Man Saves The World/Assets/Scripts/Collectable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HandleCollected();
+    }
+ 
+    // Update is called once per frame
+        void HandleCollected()
+        {
+        CollectableManger.Instance.HandleCoinCollected();
+        print("Hello darkness, my old friend I've come to talk with you again");
+        Destroy(gameObject);
+
+        }
+    
+}
---
+++ Yeet Man Saves The World/Assets/Scripts/CollectableManger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectableManger : MonoBehaviour
+{
+    private int _totalCoins;
+    private int _collectedCoins;
+
+    public TextMeshProUGUI coinCount;
+
+    public static CollectableManger Instance;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        _totalCoins = FindObjectsOfType<Collectable>().Length;
+        DisplayCoinCount();
+    }
+    void DisplayCoinCount()
+    {
+        coinCount.SetText(_collectedCoins + " / " + _totalCoins);
+        if(_collectedCoins >= _totalCoins)
+        {
+            coinCount.SetText("You Win!");
+        }
+    }
+
+    public void HandleCoinCollected()
+    {
+        _collectedCoins++;
+        DisplayCoinCount();
+    }
+}
